e7615e7ad93b183e0624db394bb45cf36f07c0b0
wartman <faxmaster@gmail.com>
2019-07-17 20:26:47 -0500

    fixed issue with stateful widget where patch would be called too soon and break events and other things

--- src/pilot/StatefulWidget.hx
+++ src/pilot/StatefulWidget.hx
@@ -13,24 +13,34 @@ class StatefulWidget implements Widget {
 
   #if js
     
+    var _pilot_ready:Bool = false;
     var _pilot_vnode:VNode;
 
     public function render():VNode {
       _pilot_vnode = build();
-      _pilot_vnode.hooks.attach = attached;
+      _pilot_vnode.hooks.attach = _pilot_attached;
       _pilot_vnode.hooks.detach = _pilot_detached;
       return _pilot_vnode;
     }
 
     public function patch() {
-      if (_pilot_vnode == null) return;
-      if (_pilot_vnode.node == null) return;
+      if (
+        !_pilot_ready
+        || _pilot_vnode == null
+        || _pilot_vnode.node == null
+      ) return;
       _pilot_vnode.node.patch(render());
     }
 
     final function _pilot_detached() {
       _pilot_vnode = null;
+      _pilot_ready = false;
       detached();
+    }
+
+    final function _pilot_attached(vnode:VNode) {
+      _pilot_ready = true;
+      attached(vnode);
     }
 
     public function attached(vnode:VNode) {
