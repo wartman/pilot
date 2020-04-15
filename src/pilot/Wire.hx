47dded0670392ad807b0619ae402e6785c6c00af
wartman <faxmaster@gmail.com>
2020-04-15 13:10:51 -0500

    use spaces

--- src/pilot/Wire.hx
+++ src/pilot/Wire.hx
@@ -2,12 +2,12 @@ package pilot;
 
 interface Wire<Node, Attrs:{}> {
   public function __getNodes():Array<Node>;
-	public function __update(
-		attrs:Attrs,
-		?children:Array<VNode>,
-		context:Context<Node>,
-		parent:Component,
-		effectQueue:Array<()->Void>
-	):Void;
-	public function __destroy():Void;
+  public function __update(
+    attrs:Attrs,
+    ?children:Array<VNode>,
+    context:Context<Node>,
+    parent:Component,
+    effectQueue:Array<()->Void>
+  ):Void;
+  public function __destroy():Void;
 }
